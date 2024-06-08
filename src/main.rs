546f99711d4e5a3561b3c2e387aca4c23bafa31a
Richard Obaze <richardobaze@yahoo.co.uk>
2024-06-08 02:36:25 +0100

    Getting data from api

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use reqwest::Error;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Company {
+    login: String,
+    id: u32,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let request_url = format!(
+        "https://api.github.com/repos/{owner}/{repo}/stargazers",
+        owner = "rust-lang-nursery",
+        repo = "rust-cookbook"
+    );
+    println!("{}", request_url);
+    let response = reqwest::get(&request_url).await?;
+
+    let users: Vec<Company> = response.json().await?;
+    println!("{:?}", users);
+    Ok(())
+}
