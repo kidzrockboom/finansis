4fdbf737d2be9e1e828568524f62cb4a3dec8d70
Richard Obaze <richardobaze@yahoo.co.uk>
2024-06-08 02:43:45 +0100

    Getting data from api

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
+use dotenv::dotenv;
 use reqwest::Error;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 struct Company {
-    login: String,
-    id: u32,
+    symbol: Option<String>,
+    name: Option<String>,
+    currency: Option<String>,
+    stock_exchange: Option<String>,
+    exchange_short_name: Option<String>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    dotenv().ok();
+
+    let path = "data.txt";
+    let mut writer = File::create(path).unwrap();
+
     let request_url = format!(
-        "https://api.github.com/repos/{owner}/{repo}/stargazers",
-        owner = "rust-lang-nursery",
-        repo = "rust-cookbook"
+        "https://financialmodelingprep.com/api/v3/search?query={query}&apikey={apiKey}",
+        query = "AA",
+        apiKey = std::env::var("FINANCIALMODELINGPREP_API_KEY").expect("Api key goes here")
     );
     println!("{}", request_url);
     let response = reqwest::get(&request_url).await?;
 
-    let users: Vec<Company> = response.json().await?;
-    println!("{:?}", users);
+    let companies: Vec<Company> = response.json().await?;
+    serde_json::to_writer(&mut writer, &companies).unwrap();
+    println!("{:?}", companies);
     Ok(())
 }
