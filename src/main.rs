d7feaaa86cca678807729df9a188e5db621d41e2
Richard Obaze <richardobaze@yahoo.co.uk>
2024-06-15 21:01:34 +0100

    Adding routes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use axum::{
-    http::StatusCode,
-    routing::{get, post},
-    Json, Router,
-};
+use axum::{http::StatusCode, routing::get, Json, Router};
+use finansis::routes::get_companies::get_companies;
 use serde::{Deserialize, Serialize};
 
 #[tokio::main]
@@ -15,7 +12,7 @@ async fn main() {
         // `GET /` goes to `root`
         .route("/", get(root))
         // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+        .route("/companies", get(get_companies));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -55,4 +52,3 @@ struct User {
     id: u64,
     username: String,
 }
-
--- src/routes/get_companies.rs
+++ src/routes/get_companies.rs
@@ -1,17 +1,24 @@
-use axum::{
-    http::StatusCode,
-    routing::{get, post},
-    Json, Router,
-};
-use serde::{Deserialize, Serialize};
+use axum::{debug_handler, http::StatusCode, Json};
+use std::{fs::File, io::BufReader};
+use tracing_subscriber::field::debug;
 
 use crate::api::finance_data::Company;
 
-fn read_json() -> Json<Company> {
-    todo!("Replace this with database call");
-    let companies: Vec<Company> = serde_json
+fn read_json() -> Vec<Company> {
+    // Replace this with database call
+    let path = "data.txt";
+
+    let file = File::open(path).expect("Cannot read file");
+    let reader = BufReader::new(file);
+
+    let companies: Vec<Company> =
+        serde_json::from_reader(reader).expect("Cannot parse json from file");
+
+    companies
 }
 
-pub fn get_companies() -> (StatusCode, Json<Company>) {
-    
+pub async fn get_companies() -> (StatusCode, Json<Vec<Company>>) {
+    let companies = read_json();
+
+    (StatusCode::OK, Json(companies))
 }
